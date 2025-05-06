1e5b7d509a622913a10bdb84cf72853e98f84b9a
Andranik Torgomyan <and.torgomyan01@gmail.com>
2025-05-07 00:08:05 +0400

    Fix bugs

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function $(target){
+  return document.querySelectorAll(target);
+}
+
+function $el(target){
+  return document.querySelector(target);
+}
+
+
+// AOS.init({
+//   duration: 1000
+// })
+//
---
+++ tailwind.config.js
@@ -0,0 +1,96 @@
+const flexClasses = {
+  '.flex-jsb-c': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  '.flex-jsb-s': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'start',
+  },
+  '.flex-jsb-e': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'end',
+  },
+  '.flex-js-c': {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  '.flex-js-s': {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'start',
+  },
+  '.flex-js-e': {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'end',
+  },
+  '.flex-jc-c': {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  '.flex-jc-s': {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'start',
+  },
+  '.flex-jc-e': {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'end',
+  },
+  '.flex-jse-c': {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  '.flex-jsa-c': {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  '.flex-je-c': {
+    display: 'flex',
+    justifyContent: 'end',
+    alignItems: 'center',
+  },
+}
+
+module.exports = {
+  mode: 'jit',
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        'white': '#FFF',
+        'black': '#11181C',
+        'gray': '#687076',
+        'blue': '#1150CB',
+        'orange': '#FF7A00',
+        'purple': '#6A8AFF',
+        'borderColor': '#D7DBDF',
+      },
+      transitionDuration: {
+        DEFAULT: '300ms',
+      },
+      borderColor: {
+        DEFAULT: "#D7DBDF"
+      },
+    },
+  },
+  variants: {
+    display: ['show'],
+  },
+  plugins: [
+    function ({ addComponents }) {
+
+      addComponents(flexClasses);
+    },
+  ],
+};
